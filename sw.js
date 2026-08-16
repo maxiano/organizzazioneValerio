99e64989161528196c6315f4ab0c3ed48d3ddc15
maxiano <maxiano@users.noreply.github.com>
2026-08-16 11:59:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,3 @@
-Il codice del Service Worker è ben scritto, ma la causa del blocco dell'installazione sta nel primo elemento dell'array ASSETS_TO_CACHE: './'.
-
-Se nell'array metti './', l'istruzione cache.addAll() proverà a fare una richiesta HTTP separata a quella cartella. Se il server hosting risponde con un reindirizzamento (es. da / a /index.html) o se una qualsiasi delle risorse fallisce nel caricamento (es. il link del CDN o un file CSS/JS con un percorso leggermente diverso), l'installazione del Service Worker fallisce silenziosamente, bloccando l'intero banner PWA.
-
-Ecco il file sw.js corretto e ottimizzato per evitare fallimenti nel caching e garantire che Chrome registri la PWA al primo colpo:
-
-JavaScript
 const CACHE_NAME = 'turni-valerio-v2';
 const ASSETS_TO_CACHE = [
   './index.html',
