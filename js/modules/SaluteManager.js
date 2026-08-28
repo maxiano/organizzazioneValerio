12a5d214f046844e820fc4a2e3c9e6e11d40add9
maxiano <maxiano@users.noreply.github.com>
2026-08-28 12:45:02 +0200

    Update SaluteManager.js

--- js/modules/SaluteManager.js
+++ js/modules/SaluteManager.js
@@ -1,15 +1,40 @@
 export class SaluteManager {
   constructor() {
-    this.schede = {}; // Contiene dati sanitari: { farmaci: [], visite: [] }
+    this.schede = {
+      pediatra: { nome: '', telefono: '', orari: '' },
+      infoGenerali: { gruppoSanguigno: '', allergie: '', terapie: '' },
+      visite: []
+    };
   }
 
-  setSchede(data = {}) {
-    this.schede = data;
+  setSchede(data) {
+    if (data) {
+      this.schede = {
+        pediatra: data.pediatra || { nome: '', telefono: '', orari: '' },
+        infoGenerali: data.infoGenerali || { gruppoSanguigno: '', allergie: '', terapie: '' },
+        visite: data.visite || []
+      };
+    }
   }
 
-  addVisita(dateKey, tipo, medico, note = '') {
-    if (!this.schede[dateKey]) this.schede[dateKey] = [];
-    this.schede[dateKey].push({ tipo, medico, note });
-    return this.schede;
+  updatePediatra(nome, telefono, orari) {
+    this.schede.pediatra = { nome, telefono, orari };
+  }
+
+  updateInfoGenerali(gruppoSanguigno, allergie, terapie) {
+    this.schede.infoGenerali = { gruppoSanguigno, allergie, terapie };
+  }
+
+  addVisita(data, descrizione, esito) {
+    this.schede.visite.push({
+      id: Date.now().toString(),
+      data,
+      descrizione,
+      esito
+    });
+  }
+
+  deleteVisita(id) {
+    this.schede.visite = this.schede.visite.filter(v => v.id !== id);
   }
 }
