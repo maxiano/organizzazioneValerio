21dee6cb05519db32fdf429dde157174a4b5e48b
maxiano <maxiano@users.noreply.github.com>
2026-08-28 23:04:13 +0200

    Update VacanzeManager.js

--- js/modules/VacanzeManager.js
+++ js/modules/VacanzeManager.js
@@ -1,50 +1,24 @@
-export class SpeseManager {
+export class VacanzeManager {
   constructor() {
-    this.spese = []; 
+    this.vacanze = [];
   }
 
-  setSpese(speseData = []) {
-    this.spese = speseData || [];
+  setVacanze(data) {
+    this.vacanze = data || [];
   }
 
-  addSpesa(descrizione, importo, pagatoDa, categoria = 'scuola', ricevutaBase64 = null, data = new Date()) {
-    const nuovaSpesa = {
-      id: 'spesa_' + Date.now().toString(),
-      data: typeof data === 'string' ? data : data.toISOString().split('T')[0],
-      descrizione,
-      importo: parseFloat(importo),
-      pagatoDa, // 'papa' o 'mamma'
-      categoria, // 'scuola', 'salute', 'sport', 'altro'
-      ricevuta: ricevutaBase64, // Stringa base64 dell'immagine
-      approvato: true
+  addVacanzeBlock(titolo, dataInizio, dataFine, assegnatoA) {
+    const newBlock = {
+      id: Date.now().toString(),
+      titolo,
+      dataInizio,
+      dataFine,
+      assegnatoA // 'papa' o 'mamma'
     };
-    this.spese.push(nuovaSpesa);
-    return this.spese;
+    this.vacanze.push(newBlock);
   }
 
-  deleteSpesa(id) {
-    this.spese = this.spese.filter(s => s.id !== id);
-    return this.spese;
-  }
-
-  calculateSaldo() {
-    let totalePapa = 0;
-    let totaleMamma = 0;
-
-    this.spese.forEach(spesa => {
-      if (spesa.pagatoDa === 'papa') totalePapa += spesa.importo;
-      if (spesa.pagatoDa === 'mamma') totaleMamma += spesa.importo;
-    });
-
-    const totaleGenerale = totalePapa + totaleMamma;
-    const quotaSpettante = totaleGenerale / 2;
-    const differenza = totalePapa - quotaSpettante; // Positivo = Papà deve ricevere, Negativo = Papà deve dare
-
-    if (differenza > 0) {
-      return { debitore: 'mamma', creditore: 'papa', importo: differenza, totalePapa, totaleMamma };
-    } else if (differenza < 0) {
-      return { debitore: 'papa', creditore: 'mamma', importo: Math.abs(differenza), totalePapa, totaleMamma };
-    }
-    return { debitore: null, creditore: null, importo: 0, totalePapa, totaleMamma };
+  deleteVacanzeBlock(id) {
+    this.vacanze = this.vacanze.filter(v => v.id !== id);
   }
 }
