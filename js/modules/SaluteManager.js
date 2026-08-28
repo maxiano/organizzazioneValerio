5cf283ffada20d1fc82bd94207b2b50292ecee32
maxiano <maxiano@users.noreply.github.com>
2026-08-28 12:57:38 +0200

    Update SaluteManager.js

--- js/modules/SaluteManager.js
+++ js/modules/SaluteManager.js
@@ -2,21 +2,24 @@ export class SaluteManager {
   constructor() {
     this.schede = {
       pediatra: { nome: '', telefono: '', orari: '' },
+      contattiUtili: [], // [{ id, nome, ruolo, telefono }]
       infoGenerali: { gruppoSanguigno: '', allergie: '', terapie: '' },
-      visite: []
+      farmaci: [],        // [{ id, nome, orario, note, somministrato: { 'YYYY-MM-DD': true/false } }]
+      visite: []         // [{ id, data, descrizione, note }]
     };
   }
 
   setSchede(data) {
-    if (data) {
-      this.schede = {
-        pediatra: data.pediatra || { nome: '', telefono: '', orari: '' },
-        infoGenerali: data.infoGenerali || { gruppoSanguigno: '', allergie: '', terapie: '' },
-        visite: data.visite || []
-      };
-    }
+    this.schede = {
+      pediatra: data.pediatra || { nome: '', telefono: '', orari: '' },
+      contattiUtili: data.contattiUtili || [],
+      infoGenerali: data.infoGenerali || { gruppoSanguigno: '', allergie: '', terapie: '' },
+      farmaci: data.farmaci || [],
+      visite: data.visite || []
+    };
   }
 
+  // GESTIONE PEDIATRA E INFO
   updatePediatra(nome, telefono, orari) {
     this.schede.pediatra = { nome, telefono, orari };
   }
@@ -25,12 +28,51 @@ export class SaluteManager {
     this.schede.infoGenerali = { gruppoSanguigno, allergie, terapie };
   }
 
-  addVisita(data, descrizione, esito) {
+  // GESTIONE FARMACI / TERAPIE
+  addFarmaco(nome, orario, note) {
+    const nuovo = {
+      id: Date.now().toString(),
+      nome,
+      orario,
+      note,
+      somministrato: {} // Mappa dateKey -> boolean (es. { "2026-08-28": true })
+    };
+    this.schede.farmaci.push(nuovo);
+  }
+
+  toggleSomministrazione(farmacoId, dateKey) {
+    const farmaco = this.schede.farmaci.find(f => f.id === farmacoId);
+    if (farmaco) {
+      if (!farmaco.somministrato) farmaco.somministrato = {};
+      farmaco.somministrato[dateKey] = !farmaco.somministrato[dateKey];
+    }
+  }
+
+  deleteFarmaco(id) {
+    this.schede.farmaci = this.schede.farmaci.filter(f => f.id !== id);
+  }
+
+  // GESTIONE CONTATTI UTILI
+  addContatto(nome, ruolo, telefono) {
+    this.schede.contattiUtili.push({
+      id: Date.now().toString(),
+      nome,
+      ruolo,
+      telefono
+    });
+  }
+
+  deleteContatto(id) {
+    this.schede.contattiUtili = this.schede.contattiUtili.filter(c => c.id !== id);
+  }
+
+  // GESTIONE VISITE
+  addVisita(data, descrizione, note = '') {
     this.schede.visite.push({
       id: Date.now().toString(),
       data,
       descrizione,
-      esito
+      note
     });
   }
 
